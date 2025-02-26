17dda080ad83ab6267ea1a4754f05293250110b3
Maki Spariva <makspariva@gmail.com>
2025-02-26 14:21:20 +0100

    clase

--- Controllers/CubosController.cs
+++ Controllers/CubosController.cs
@@ -18,5 +18,26 @@ namespace MvcNetCoreTiendaCubos.Controllers
             List<Cubo> cubos = await this.repo.GetCubosAsync();
             return View(cubos);
         }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Cubo cubo = await this.repo.FindCuboAsync(id);
+            return View(cubo);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string nombre, string modelo, string marca, string imagen, int precio)
+        {
+            int idCubo = await this.repo.GetMaxIdUser();
+            await this.repo.InsertCuboAsync(idCubo, nombre, modelo, marca, imagen, precio);
+            return RedirectToAction("Details", new { id = idCubo });
+        }
+
+
     }
 }
--- Repositories/RepositoryCubos.cs
+++ Repositories/RepositoryCubos.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using MvcNetCoreTiendaCubos.Data;
 using MvcNetCoreTiendaCubos.Models;
 
@@ -6,6 +7,15 @@ namespace MvcNetCoreTiendaCubos.Repositories
 {
     public class RepositoryCubos
     {
+        #region Vistas y procedures
+        //        CREATE VIEW V_COMPRADETALLE
+        //as 
+        //	select cubos.nombre as nombrecubo, cubos.precio as preciocubo, compra.cantidad,
+        //    compra.precio as preciocompra, compra.fechapedido
+        //    from cubos inner join compra
+        //    on cubos.id_cubo = compra.id_cubo;
+        #endregion
+
         private CubosContext context;
         public RepositoryCubos(CubosContext context)
         {
@@ -26,5 +36,33 @@ namespace MvcNetCoreTiendaCubos.Repositories
                            select datos;
             return await consulta.FirstOrDefaultAsync();
         }
+
+        public async Task<int> GetMaxIdUser()
+        {
+            if (this.context.cubos.Count() == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return await this.context.cubos.MaxAsync
+                    (x => x.IdCubo) + 1;
+            }
+        }
+
+        public async Task InsertCuboAsync(int idCubo, string nombre, string modelo, string marca, string imagen, int precio)
+        {
+            Cubo cubo = new Cubo();
+            cubo.IdCubo = idCubo;
+            cubo.Nombre = nombre;
+            cubo.Modelo = modelo;
+            cubo.Marca = marca;
+            cubo.Imagen = imagen;
+            cubo.Precio = precio;
+            this.context.cubos.Add(cubo);
+            await this.context.SaveChangesAsync();
+        }
+
+
     }
 }
